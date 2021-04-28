6bedc3a1f2ca7de06d450e7afd7364cac0e1403e
Aragar199 <avponce@hotmail.com>
2021-04-28 14:04:20 -0700

    Add MyContract, Integer, Variables solidity example contracts

---
+++ IntegerExample.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.4;
+
+contract IntegerExample {
+    uint public myUint;
+    
+    function setMyUint(uint _myUint) public {
+        myUint = _myUint;
+    }
+}
